7f77608b31979062f3f8f48997346b35f102c793
Bob Cunningham <bobchm@gmail.com>
2022-07-10 10:24:42 -0400

    Made the edit dialog use Material UI

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 export default function Edit() {
     const [form, setForm] = useState({
@@ -69,65 +80,69 @@ export default function Edit() {
     
       // This following section will display the form that takes input from the user to update the data.
       return (
-        <div>
-          <h3>Update Task</h3>
-          <form onSubmit={onSubmit}>
-            <div className="form-group">
-              <label htmlFor="item">Item: </label>
-              <input
-                type="text"
-                className="form-control"
-                id="item"
-                value={form.item}
-                onChange={(e) => updateForm({ item: e.target.value })}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="due">Due: </label>
-              <input
-                type="text"
-                className="form-control"
-                id="due"
-                value={form.due}
-                onChange={(e) => updateForm({ due: e.target.value })}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="note">Note: </label>
-              <input
-                type="text"
-                className="form-control"
-                id="note"
-                value={form.note}
-                onChange={(e) => updateForm({ note: e.target.value })}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="tag">Tag: </label>
-              <input
-                type="text"
-                className="form-control"
-                id="tag"
-                value={form.tag}
-                onChange={(e) => updateForm({ tag: e.target.value })}
-              />
-            </div>
-            <br />
-    
-            <div className="form-group">
-              <input
-                type="submit"
-                value="Update Task"
-                className="btn btn-primary"
-              />
-              <input
-                type="button"
-                value="Cancel"
-                className="btn btn-primary"
-                onClick={() => navigate("/")}
-              />
-            </div>
-          </form>
-        </div>
+          <div>
+              <Box sx={{ flexGrow: 1, borderRadius: "10px" }}>
+                  <AppBar
+                        position="static"
+                        alignItems="flex-start"
+                        sx={{ borderRadius: "10px 10px 0px 0px" }}>
+                        <Toolbar>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                Update Task
+                            </Typography>
+                        </Toolbar>
+                  </AppBar>
+              </Box>
+              <Paper
+                  sx={{
+                      bgcolor: 'background.paper',
+                      boxShadow: 2,
+                      borderRadius: "10px",
+                  }}
+              >
+                  <Stack 
+                    className="container" 
+                    direction="column" 
+                    alignItems="flex-start" 
+                    justifyContent="flex-start" 
+                    sx={{maxHeight: 200}}
+                    spacing={2}>
+                      <TextField
+                          value={form.item}
+                          onChange={(e) => updateForm({ item: e.target.value })}
+                          label={"Task"}
+                      />
+                      <TextField
+                          value={form.due}
+                          onChange={(e) => updateForm({ due: e.target.value })}
+                          label={"Due"}
+                      />
+                      <TextField
+                          value={form.note}
+                          onChange={(e) => updateForm({ note: e.target.value })}
+                          label={"Note"}
+                      />
+                      <TextField
+                          value={form.tag}
+                          onChange={(e) => updateForm({ tag: e.target.value })}
+                          label={"Tag"}
+                      />
+                      <Stack direction="row" alignItems="flex-start" justifyContent="flex-start" spacing={2}>
+                          <Button
+                              onClick={event => onSubmit(event)}
+                              variant="contained"
+                              startIcon={<SaveIcon />}>
+                              Save
+                          </Button>
+                          <Button
+                              onClick={() => navigate("/")}
+                              variant="contained"
+                              startIcon={<CancelIcon />}>
+                              Cancel
+                          </Button>
+                      </Stack>
+                  </Stack>
+              </Paper>
+          </div>
       );
     }
